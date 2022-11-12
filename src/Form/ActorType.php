a61246eac1890fb41f2c77b87e17727809d8521e
Ismail T <takkalismael@gmail.com>
2022-11-12 03:59:11 +0100

    formTypes modifications

--- src/Form/ActorType.php
+++ src/Form/ActorType.php
@@ -4,18 +4,33 @@ namespace App\Form;
 
 use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ActorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('birthday')
-            ->add('gender')
+            ->add('firstname', TextType::class, [
+                "label" => "prénom ",
+            ])
+            ->add('lastname', TextType::class, ['label' => "nom "])
+            ->add('birthday', DateType::class, [
+                'widget' => 'single_text',
+                'label' => "date de naissance ",
+            ])
+            ->add('gender', ChoiceType::class, [
+                "choices" => [
+                    "homme" => "h",
+                    "femme" => "f",
+                    "autres" => "x",
+                ],
+                "label" => "genre ",
+            ])
             ->add('movies')
         ;
     }
